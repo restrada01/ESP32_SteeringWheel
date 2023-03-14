eb86571e66ce382f1f4c252bceade46893414dee
restrada01 <78570415+restrada01@users.noreply.github.com>
2023-03-14 14:54:51 -0400

    Testing SPIFFS upload with GPIO output

--- ESP32_AccessPoint_SPIFFS.ino
+++ ESP32_AccessPoint_SPIFFS.ino
@@ -1,18 +1,69 @@
 // libraries for using ESP32 as Access Point (outputs its own network)
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
 
 // set network details
 const char* ssid = "our_ssid";
 const char* password = "our_secret_password";
 
+// initialize web server on port 80
 AsyncWebServer server(80);
 
+// configure GPIO pin 27 for LED output to test
+// Set LED GPIO
+const int ledPin = 27;
+// Stores LED state
+String ledState;
+
+// Replaces placeholder with LED state value
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if(digitalRead(ledPin)){
+      ledState = "ON";
+    }
+    else{
+      ledState = "OFF";
+    }
+    Serial.print(ledState);
+    return ledState;
+  }
+  return String();
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  
+  // initialize ESP for softAP and print IP for browser
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
   Serial.println(IP);
+  
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to set GPIO to HIGH
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, HIGH);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set GPIO to LOW
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // initialize server
   server.begin();
 }
 
